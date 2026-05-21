e0f8cbf1ccbdbcd61a449446e05e0059e74de35c
Tristan BAYEUX <tristan.bayeux@etu.univ-amu.fr>
2026-05-21 09:28:33 +0000

    ex2

--- src/main/java/fr/univ_amu/iut/exercice2/CompteurController.java
+++ src/main/java/fr/univ_amu/iut/exercice2/CompteurController.java
@@ -49,6 +49,7 @@ public class CompteurController {
     //
     // Ainsi, chaque fois que la valeur de `compteur` change (via incrementer/decrementer/reinit),
     // le label se met à jour automatiquement - aucun setText() à appeler !
+    labelCompteur.textProperty().bind(compteur.asString());
   }
 
   /**
@@ -60,18 +61,21 @@ public class CompteurController {
   @FXML
   private void incrementer() {
     // TODO exercice 2 : incrémenter la valeur de la propriété compteur.
+    compteur.set(compteur.get() + 1);
   }
 
   /** Action du bouton −. Décrémente le compteur. */
   @FXML
   private void decrementer() {
     // TODO exercice 2 : décrémenter la valeur de la propriété compteur.
+    compteur.set(compteur.get() - 1);
   }
 
   /** Action du bouton Réinitialiser. Remet le compteur à zéro. */
   @FXML
   private void reinitialiser() {
     // TODO exercice 2 : remettre la propriété compteur à 0.
+    compteur.set(0);
   }
 
   /**
